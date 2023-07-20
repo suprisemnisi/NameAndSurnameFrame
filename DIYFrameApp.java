2d512d6ab8407a1f01f3b9566dfd762205acfebf
suprisemnisi <140027867+suprisemnisi@users.noreply.github.com>
2023-07-20 22:38:49 +0200

    Add files via upload

---
+++ DIYFrameApp.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diyframeapp;
+
+import za.ac.tut.ui.DIYFrame;
+
+/**
+ *
+ * @author khens
+ */
+public class DIYFrameApp {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        DIYFrame frame1;//declare your frame
+   
+    frame1=new DIYFrame();
+    }
+    
+}
